963b39a676b27c28650229f6f7764ee8233de1e4
Premnath K <premnathkulal1998@gmail.com>
2024-12-21 14:15:24 +0530

    Updated pdf view in scroll view

--- src/pages/home-page/Home.tsx
+++ src/pages/home-page/Home.tsx
@@ -2,20 +2,22 @@ import "./Home.scss";
 import NewsPdf from "../../assets/Vijay-Karnataka.pdf";
 import { useEffect, useState } from "react";
 import { Document, Page } from "react-pdf";
-import HTMLFlipBook from "react-pageflip";
 
 const Home = () => {
   const [file] = useState(NewsPdf);
   const [numPages, setNumPages] = useState(null);
   const [pageWidth, setPageWidth] = useState(500);
   const [pageHeight, setPageHeight] = useState(790);
+  const [isMobile, setIsMobile] = useState(true);
 
   useEffect(() => {
     const updatePageSize = () => {
+      setIsMobile(true);
       let containerWidth = window.innerWidth;
       let containerHeight = window.innerHeight;
 
-      if (!(window.innerWidth < 500)) {
+      if (!(window.innerWidth < 1000)) {
+        setIsMobile(false);
         containerWidth = window.innerWidth * 0.8;
         containerHeight = window.innerHeight * 0.8;
         return;
@@ -43,51 +45,23 @@ const Home = () => {
   return (
     <div className="home">
       <div className="new-paper-container">
-        <HTMLFlipBook
-          width={pageWidth}
-          height={pageHeight}
-          showCover={true}
-          className=""
-          startPage={1}
-          size="fixed"
-          minWidth={pageWidth}
-          maxWidth={pageWidth}
-          minHeight={pageHeight}
-          maxHeight={pageHeight}
-          maxShadowOpacity={0.5}
-          mobileScrollSupport={true}
-          drawShadow={true}
-          flippingTime={1000}
-          usePortrait={true}
-          startZIndex={0}
-          autoSize={true}
-          clickEventForward={true}
-          useMouseEvents={true}
-          swipeDistance={30}
-          style={{
-            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-          }}
-          showPageCorners={false}
-          disableFlipByClick={false}
-        >
-          {Array.from(new Array(numPages), (_, index) => (
-            <div key={index}>
-              <Document
-                file={file}
-                onLoadSuccess={onDocumentLoadSuccess}
-                loading={<div>Loading PDF...</div>}
-              >
-                <Page
-                  pageNumber={index + 1}
-                  renderTextLayer={false}
-                  renderAnnotationLayer={false}
-                  width={pageWidth}
-                  height={pageHeight}
-                />
-              </Document>
-            </div>
-          ))}
-        </HTMLFlipBook>
+        {Array.from(new Array(numPages), (_, index) => (
+          <div key={index}>
+            <Document
+              file={file}
+              onLoadSuccess={onDocumentLoadSuccess}
+              loading={index === 0 && <div>Loading PDF...</div>}
+            >
+              <Page
+                pageNumber={index + 1}
+                renderTextLayer={false}
+                renderAnnotationLayer={false}
+                width={isMobile ? pageWidth : pageWidth * 2}
+                height={pageHeight}
+              />
+            </Document>
+          </div>
+        ))}
       </div>
     </div>
   );
